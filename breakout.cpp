8cbdcc4ef766cddb452c71fd120bccfa8518b632
Davi Wendt <daviwendt@gmail.com>
2020-09-14 00:02:33 -0300

    Added mouse movement with lerp

--- breakout.cpp
+++ breakout.cpp
@@ -10,6 +10,7 @@ public:
 private:
 	float fBatPos{ 20.0f };
 	float fBatWidth{ 40.0f };
+	float fBatSpeed{ 0.5f };
 
 	olc::vf2d vBall{ 200.0f, 200.0f };
 	float fBallRadius{ 5.0f };
@@ -20,14 +21,22 @@ public:
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override{
+		// Clears screen
+		Clear(olc::DARK_BLUE);
+
 		// Handle User Input
-		fBatPos = static_cast<float>(GetMouseX());
+		fBatPos += (fElapsedTime * (static_cast<float>(GetMouseX()) - fBatPos)) * fBatSpeed;
 
 		if (fBatPos < 11.0f) fBatPos = 11.0f;
 		if (fBatPos + fBatWidth > static_cast<float>(ScreenWidth()) - 10.0f)
 			fBatPos = static_cast<float>(ScreenWidth()) - 10.0f - fBatWidth;
 
 		// Debuggin
+		if (GetMouse(0).bHeld){
+			if (GetMouseWheel() > 0) fBatSpeed += 0.1f;
+			if (GetMouseWheel() < 0) fBatSpeed -= 0.1f;	
+		}
+		
 		if (GetMouse(1).bHeld){
 			vBall = { static_cast<float>(GetMouseX()), static_cast<float>(GetMouseY()) };
 			if (GetMouseWheel() > 0) fBallRadius += 1.0f;
@@ -37,8 +46,6 @@ public:
 		if (GetMouse(2).bPressed) fBallRadius = 5.0f;
 
 
-		// Erase previous frame
-		Clear(olc::DARK_BLUE);
 
 		// Draw Boundary
 		DrawLine(10, 10, ScreenWidth() - 10, 10, olc::YELLOW);
@@ -49,7 +56,7 @@ public:
 		FillRect(int(fBatPos), ScreenHeight() - 20, int(fBatWidth), 10, olc::GREEN);
 
 		// Draw Ball
-		FillCircle(vBall, static_cast<int>(fBallRadius), olc::CYAN);
+		DrawCircle(vBall, static_cast<int>(fBallRadius), olc::CYAN);
 		return true;
 	}
 };
